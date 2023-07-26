7c66373f51c1a50908d8fa0cf34b770ac8aef189
Karissa Sanchez <kasanchez519@gmail.com>
2023-07-26 22:23:11 +0000

    create a setup.py file
    Signed-off-by: Karissa Sanchez <kasanchez519@gmail.com>

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+  
+with open("README.md", "r") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="guest-agent",
+    version="0.0.1",
+    author="Karissa Sanchez",
+    author_email="kasancehz519@gmail.com",
+    packages=["src/guest_agent", "test"],
+    description="A guest agent for cloud hypervisor VMs",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kasanchez519/ch-guest-agent.git",
+    license='BSD-2-Clause',
+    python_requires='>=3.8',
+    install_requires=[]
+)
